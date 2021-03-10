5889f64969af91a63f91159a2a2db8f5920089f1
Viseko <viseko23@gmail.com>
2021-03-11 00:42:45 +0700

    Вёрстка секции "Отзывы"

---
+++ source/js/_reviews-slider.js
@@ -0,0 +1,27 @@
+const reviewSlider = new Swiper('.reviews-slider__container', {
+  loop: true,
+  wrapperClass: 'reviews-slider__list',
+
+  pagination: {
+    el: '.reviews-slider__pagination',
+    bulletClass: 'reviews-slider__bullet',
+    bulletActiveClass: 'reviews-slider__bullet--active',
+    clickable: true,
+  }
+});
+
+
+// Костыль, сделанный с целью назначить кнопки навигации за пределами слайдер-контейнера.
+
+// Выход не очень элегантный, но другого способа сделать в десктопном слайдере отсупы и чтобы кнопки располагались за контейнером, не нашел.
+
+reviewSlider.nextBtn = document.querySelector('.reviews-slider__nav-btn--next');
+reviewSlider.prevBtn = document.querySelector('.reviews-slider__nav-btn--prev');
+
+reviewSlider.nextBtn.addEventListener("click", function() {
+  reviewSlider.slideNext();
+});
+
+reviewSlider.prevBtn.addEventListener("click", function() {
+  reviewSlider.slidePrev();
+});
--- source/js/script.js
+++ source/js/script.js
@@ -1,2 +1,4 @@
 @@include("_hand-slider.js");
 @@include("_burger.js");
+
+@@include("_reviews-slider.js");
