38d87049e3f0e568600be03f78f111a94fd5b1d4
Viseko <viseko23@gmail.com>
2021-03-13 01:26:56 +0700

    Добавил включение интерактивной карты

--- source/js/_map.js
+++ source/js/_map.js
@@ -1,32 +1,44 @@
 (function () {
-    ymaps.ready(initMap);
+    ymaps.ready(prepareMap);
+
+    let mapInit = false;
+    const mapWrapper = document.querySelector('.map__interactive');
+
+    function prepareMap() {
+        mapWrapper.addEventListener("click", initMap);
+    }
 
     function initMap() {
-        let myMap = new ymaps.Map("map-interactive", {
-            center: [59.938635, 30.323118],
-            zoom: 17,
-            controls: ["zoomControl"]
-        });
+        if (!mapInit) {
+            mapInit = true;
 
-        let myGeoObjects = [];
+            mapWrapper.classList.add('map__interactive--init');
 
-        myGeoObjects = new ymaps.Placemark([59.938635, 30.323118], {
-            baloonContentBody: 'Текст в балуне',
-        }, {
-            iconLayout: 'default#image',
-            iconImageHref: 'img/svg/map-marker.svg',
-            iconImageSize: [36,36],
-            iconImageOffset: [-18, -18]
-        });
+            let myMap = new ymaps.Map("map-interactive", {
+                center: [59.938635, 30.323118],
+                zoom: 17,
+                controls: ["zoomControl"]
+            });
 
-        let clusterer = new ymaps.Clusterer({
-            clusterDisableClickZoom: false,
-            cluseterOpenBaloonOnClick: false,
-        });
+            let myGeoObjects = [];
 
-        clusterer.add(myGeoObjects);
-        myMap.geoObjects.add(clusterer);
+            myGeoObjects = new ymaps.Placemark([59.938635, 30.323118], {
+                baloonContentBody: 'Текст в балуне',
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/svg/map-marker.svg',
+                iconImageSize: [36,36],
+                iconImageOffset: [-18, -18]
+            });
 
-        // myMap.behaviours.disable('scrollZoom');
+            let clusterer = new ymaps.Clusterer({
+                clusterDisableClickZoom: false,
+                cluseterOpenBaloonOnClick: false,
+            });
+
+            clusterer.add(myGeoObjects);
+            myMap.geoObjects.add(clusterer);
+        }
     }
+
 })();
