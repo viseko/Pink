c35aa7b8c242de304feecd3de140e0fee6e3b022
Viseko <viseko23@gmail.com>
2021-03-27 09:08:58 +0700

    Update 2021-03-27T02:08:53.289Z

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-let nojs = document.querySelector('.nojs');
+let nojs = document.querySelector('html.nojs');
 
 if (nojs) {
-  nojs.classList.remove('nojs')
+  nojs.removeAttribute('class');
+
 };
-;
+// Взято здесь: https://gist.github.com/Protoff/d6643387f03d47b44b2d7c3cf7b3e0a0
+
+document.addEventListener('DOMContentLoaded', function() {
+  testWebP(document.body)
+})
+
+function testWebP(elem) {
+  const webP = new Image();
+  webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+  webP.onload = webP.onerror = function () {
+    webP.height === 2 ? elem.classList.add('webp') : elem.classList.add('no-webp')
+  }
+};
 
 const handSlider = new Swiper('.hand-slider__screen-wrapper', {
   loop: true,
