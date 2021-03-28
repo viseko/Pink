7e6a9a430335858976f60919f65f87d5bf877b11
Viseko <viseko23@gmail.com>
2021-03-28 20:02:53 +0700

    Ретинизация графики, мелкие правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,26 @@
+// Отключение nojs у документа
 @@include("_no-js.js");
+
+// Проверка клиента на поддержку webp
 @@include("_webp.js");
 
-@@include("_hand-slider.js");
+// Оживление мобильного меню
 @@include("_burger.js");
 
+// Подключение swiper для слайдера в промо-блоке
+@@include("_hand-slider.js");
+
+// Подключение swiper для слайдера с отзывами
 @@include("_reviews-slider.js");
 
+// Подключение swiper для мобильной версии таблицы тарифов
 @@include("_tariffs-slider.js");
 
+// Подключение яндекс-карты
 @@include("_map.js");
 
+// Табы для мобильной версии формы отправки фото
 @@include("_photo-post-tabs.js");
 
+// Скрипт для валидации и отправки формы на странице form.html
 @@include("_form-validation.js");
